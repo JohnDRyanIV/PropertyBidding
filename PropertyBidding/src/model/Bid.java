728ce2935d5819bda3a34718c6ce4ceec95dae25
JohnDRyanIV <43046528+JohnDRyanIV@users.noreply.github.com>
2020-10-16 16:14:51 -0700

    Further implemented model classes. Bid might need more work, others good

--- PropertyBidding/src/model/Bid.java
+++ PropertyBidding/src/model/Bid.java
@@ -20,7 +20,7 @@ public class Bid {
 	private int id;
 	
 	@Column(name="BID_AMOUNT")
-	private double amount;
+	private Double amount;
 	
 	@ManyToOne (cascade=CascadeType.PERSIST) // Might not be many to one, check later
 	@JoinColumn(name="PROP_ID")
@@ -34,7 +34,52 @@ public class Bid {
 		super();
 	}
 	
-	// Implement constructor, getters/setters, and toString
+	public Bid(Double amount, Property property, Bidder bidder) {
+		super();
+		setAmount(amount);
+		setProperty(property);
+		setBidder(bidder);
+	}
+	
+	// Getters-Setters & toString
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Property getProperty() {
+		return property;
+	}
+
+	public void setProperty(Property property) {
+		this.property = property;
+	}
+
+	public Bidder getBidder() {
+		return bidder;
+	}
+
+	public void setBidder(Bidder bidder) {
+		this.bidder = bidder;
+	}
+
+	@Override
+	public String toString() {
+		return "Bid [id=" + id + ", amount=" + amount + ", property=" + property.toString() + 
+				", bidder=" + bidder.toString() + "]";
+	}
 	
 
 }
