6749979fe05a68aaf539a965d92627a075ed9d01
Bob Fox <robertfox8@msn.com>
2020-10-18 13:53:17 -0500

    RF - edit bid components

--- PropertyBidding/WebContent/edit-bid.jsp
+++ PropertyBidding/WebContent/edit-bid.jsp
@@ -1,12 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Edit Bid Amount</title>
 </head>
 <body>
-
+	<form action = "editBidServlet" method = "post">
+		Bidder: ${bidToEdit.getBidder().getName()}  <br/>
+		Address: "${bidToEdit.getProperty().getAddress()}"><p></p>
+		City: "${bidToEdit.getProperty().getCity()}"><p></p>
+		State: "${bidToEdit.getProperty().getState}"><p></p>
+		Zipcode: "${bidToEdit.getProperty().getZip()}"><p></p> <br/>
+		Bid Amount: <input type = "text" name = "editAmount" value = "${bidToEdit.getAmount()}"> <br/>
+		<input type = "hidden" name = "editBidId" value = "${bidToEdit.id}">
+		<input type = "submit" value = "Save edited bid">
+	</form>
+	<a href = "index.html">Return to homepage</a> 
 </body>
 </html>
--- PropertyBidding/src/controller/EditBidServlet.java
+++ PropertyBidding/src/controller/EditBidServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Bid;
+import model.Bidder;
+
 /**
  * Servlet implementation class EditBidServlet
  */
@@ -35,7 +38,36 @@ public class EditBidServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		BidHelper dao = new BidHelper();
+		
+		// Getting the edited bid amount
+		String editedBidAmount = request.getParameter("editAmount");
+		
+		Double bidAmt;
+		
+		// get the id for the bid being edited
+		Integer tempId = Integer.parseInt(request.getParameter("editBidId"));
+	
+		try {  // get the updated bid amount
+			bidAmt = Double.parseDouble(editedBidAmount);
+		} catch (NumberFormatException e) {
+			bidAmt = null;
+			System.out.println("Invalid bid amount entered");
+		}
+		
+		// if user entered an invalid bid amount, then skip the update
+		if (bidAmt != null) {
+			// Finding bidder
+			Bid bidToUpdate = dao.searchForBidById(tempId);
+			
+			bidToUpdate.setAmount(bidAmt);
+			
+			// Updating bidder
+			dao.updateBid(bidToUpdate);
+		}
+
+		// redirect to view all bids servlet
+		getServletContext().getRequestDispatcher("/viewAllBidsServlet").forward(request, response);
 	}
 
 }
