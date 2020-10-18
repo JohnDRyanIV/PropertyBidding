fb5d9b5580b994626447f9922409525a9e6c16a7
Bob Fox <robertfox8@msn.com>
2020-10-18 13:09:07 -0500

    RF_2020-10-18b_new_bid_components

--- PropertyBidding/WebContent/new-bid.jsp
+++ PropertyBidding/WebContent/new-bid.jsp
@@ -4,9 +4,34 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>New Property Bid</title>
 </head>
 <body>
-
+	<form method = "post" action = "addBidServlet">
+	Select a Bidder:
+	<table>
+	<c:forEach items="${requestScope.selectBidder}" var="currentbidder">
+		<tr> 
+			<td><input type="radio" name="bidderId" value="${currentbidder.id}"></td>
+			<td>Bidder:  ${currentbidder.getName()}</td>
+		</tr>
+	</c:forEach>
+	</table>
+	Select a Property:
+	<table>
+	<c:forEach items="${requestScope.allProperties}" var = "currentProperty">
+		<tr>
+			<td><input type="radio" name="propId" value="${currentProperty.id}"></td>
+			<td>Address: ${currentProperty.getAddress() }</td>
+			<td>${currentProperty.getCity()}</td> <!-- city -->
+			<td>${currentPropety.getState()}</td> <!-- state -->
+			<td>${currentProperty.getZip()}</td> <!--  zipcode -->
+			<td>${currentProperty.showDateSold()}</td>
+		</tr>
+	</c:forEach>
+	</table>
+	Enter Bid Amount: <input type = "text" name = "bidAmount">
+	<input type = "submit" value = "add" name="doThisToBid">
+	</form>
 </body>
 </html>
--- PropertyBidding/src/controller/AddBidServlet.java
+++ PropertyBidding/src/controller/AddBidServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Bid;
+import model.Bidder;
+import model.Property;
+
 /**
  * Servlet implementation class AddBidServlet
  */
@@ -34,8 +38,52 @@ public class AddBidServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		BidHelper dao = new BidHelper();
+		
+		BidderHelper bho = new BidderHelper();
+		
+		PropertyHelper pho = new PropertyHelper();
+		
+		Bidder bidr = null;
+		
+		Property prop = null;
+		
+		Integer bidderId;
+		
+		Integer propertyId;
+		
+		Double bidAmount = null;
+		
+		try {  // get the bidder ID of the selected bidder
+			bidderId = Integer.parseInt(request.getParameter("bidderId"));
+			bidr = bho.searchForBidderById(bidderId);
+		} catch (NumberFormatException e) {
+			bidderId = null;
+			System.out.println("Forgot to select a bidder");
+		}
+		try {  // get the property ID of the selected property
+			propertyId = Integer.parseInt(request.getParameter("propertyId"));
+			prop = pho.searchForPropertyById(propertyId);
+		} catch (NumberFormatException e) {
+			propertyId = null;
+			System.out.println("Forgot to select a property");
+		}
+		try {  // get the bid amount that was entered
+			bidAmount = Double.parseDouble(request.getParameter("bidAmount"));
+		} catch (NumberFormatException e) {
+			bidAmount = null;
+			System.out.println("Invalid bid amount entered");
+		}
+		
+		// check to see if we are missing any input from the user.  if all three
+		// pointers are not null, then we are okay to add the bid to the database.
+		if ((bidderId != null) && (propertyId != null) && (bidAmount != null)) { 
+			Bid newBid = new Bid(bidAmount, prop, bidr);                       
+			dao.insertBid(newBid);
+		}
+		
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
 	}
 
 }
